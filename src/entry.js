0e1b91fd376ba02c7cabad6c3a91208a93744b65
你好2007 <2501482523@qq.com>
2021-08-27 20:55:32 +0800

    重新设计UI并开发完毕

--- src/entry.js
+++ src/entry.js
@@ -5,9 +5,14 @@ import '@hai2007/polyfill/Promise.js';
 
 // 引入样式
 import "@hai2007/style/normalize.css";
-import "./styles/common.css";
+import "./styles/style.scss";
 
-import App from './App.paper';
+import urlFormat from './services/urlFormat';
+
+// 启动界面
+let pages = {
+    app: () => import('./App.paper'),
+};
 
 // 打开新页面
 Knowpedia.prototype.loadPage = url => {
@@ -17,13 +22,21 @@ Knowpedia.prototype.loadPage = url => {
     aDom.click();
 };
 
-// 创建对象
-window.knowpedia = new Knowpedia({
+// 获取启动页面名称
+let pagename = urlFormat(window.location.href).router[0];
 
-    // 挂载点
-    el: document.getElementById('root'),
+// 请求页面
+(pagename in pages ? pages[pagename] : pages.app)().then(data => {
 
-    // 启动组件
-    render: createElement => createElement(App)
+    // 创建对象
+    window.knowpedia = new Knowpedia({
+
+        // 挂载点
+        el: document.getElementById('root'),
+
+        // 启动组件
+        render: createElement => createElement(data.default)
+
+    });
 
 });
